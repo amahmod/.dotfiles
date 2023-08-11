081213cb109bbca3582d02facf9ae423b3c55803
Mohammad Apel Mahmod <amahmod@pop-os.localdomain>
2023-08-11 21:19:41 +0600

    polybar: use absolute config path

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,9 +9,9 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 connected_monitor_count=$(xrandr | grep " connected " | awk "{ print$1 }" | wc -l )
 echo $connected_monitor_count
 if [ $connected_monitor_count -gt 1 ]; then
-    polybar left &
-    polybar right &
+    polybar left --config=~/.config/polybar/config.ini &
+    polybar right --config=~/.config/polybar/config.ini &
 else
-  polybar main &
+  polybar main --config=~/.config/polybar/config.ini &
 fi
 echo "Bars launched..."
